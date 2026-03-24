e473129f23920be0df15b55ad2a6ac1e7cff8ba0
Mathis Krizmanic <mathis.krizmanic@gmail.com>
2026-03-24 18:38:32 +0100

    Display articles depending of country

--- projet/src/Controller/ProductController.php
+++ projet/src/Controller/ProductController.php
@@ -7,15 +7,41 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+use App\Entity\User;
 
 final class ProductController extends AbstractController
 {
+    // A enlever?
     #[Route('/product', name: 'product_page')]
     public function index(): Response
     {
         $args = array(
             'a' => 'c',
             'b' => 'd',
+        );
+
+        return $this->render('ProductPage/product.html.twig', $args);
+    }
+
+    // Méthode pour afficher les produits sur le site de vente : 
+    // -> L'utilisateur doit etre connecté pour identifier son pays
+    // -> Une requête est faite pour ne proposer que les produits disponibles dans le pays de l'utilisateur
+
+    #[Route('/products', name: 'product_list')]
+    public function listOfProduct(ProductRepository $repo): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user instanceof User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $country = $user->getCountry();
+        $products = $repo->findByCountry($country);
+
+        $args = array(
+            'products' => $products,
         );
 
         return $this->render('ProductPage/product.html.twig', $args);
--- projet/src/Repository/ProductRepository.php
+++ projet/src/Repository/ProductRepository.php
@@ -16,28 +16,14 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    //    /**
-    //     * @return Product[] Returns an array of Product objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Product
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    // Requête qui retourne la liste des produits disponible dans un pays choisi
+    public function findByCountry($country)
+    {
+        return $this->createQueryBuilder('p')
+            ->join('App\Entity\CountryProduct', 'cp', 'WITH', 'cp.product = p')
+            ->where('cp.country = :country')
+            ->setParameter('country', $country)
+            ->getQuery()
+            ->getResult();
+    }
 }
